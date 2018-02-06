bd97cb37eda5f9eb23b05bfadfebfb0ef75ec0db
agustinezequielgomez <agusgomez99@gmail.com>
2018-02-06 11:02:34 -0300

    Clase 2

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+    var calculo;
+    importe = document.getElementById("importe").value;
+    importe = parseInt(importe);
+    calculo = parseInt(calculo);
+    calculo = importe - (importe *25 /100);
+    document.getElementById("resultado").value = calculo;
+
 }
