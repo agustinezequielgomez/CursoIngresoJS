da1e9ab5aa31d4ea78f93a0b9a86828d5a53124c
agustinezequielgomez <agusgomez99@gmail.com>
2018-02-17 10:54:48 -0300

    Clase 3

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	numero = (Math.floor(Math.random() * 11));
+	if (numero == 9 || numero == 10){
+		alert("Excelente");
+		}else if(numero < 8 && numero > 4){
+			alert("Aprobo");
+			}else{
+				alert("Vamos, la proxima se puede");
+			}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,6 +2,6 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
- (Math.random() * 11) = document.getElementById("FormIngreso");
+ alert (Math.floor(Math.random() * 11));
 
 }//FIN DE LA FUNCIÓN
