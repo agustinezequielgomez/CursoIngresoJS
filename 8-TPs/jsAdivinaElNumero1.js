1fc0284dc21e03142d289888aee9d53d11e3a88d
agustinezequielgomez <agusgomez99@gmail.com>
2018-02-19 18:17:18 -0300

    Numero con While

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,7 +9,7 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 numeroSecreto = parseFloat(numeroSecreto);
-var contador;
+var contador = 0;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
@@ -17,7 +17,7 @@ function comenzar()
 		//alert(numeroSecreto );
     contador = 0;
     document.getElementById("intentos").value = contador;
-numeroSecreto = Math.floor(Math.random() * 101);
+    numeroSecreto = Math.floor(Math.random() * 101);
   console.log ( numeroSecreto );
   
 }
@@ -28,29 +28,28 @@ function verificar()
   var confirmar;
   numeroUsuario = document.getElementById("numero").value;
   numeroUsuario = parseFloat(numeroUsuario);
-	  if (contador >= 10){
+    if (contador >= 10){
     alert("Te pasaste de 10 intentos");
-      }else if (numeroSecreto == numeroUsuario && contador == 0 && numeroSecreto < 100){
-      contador = contador + 1;
-      numeroSecreto = numeroSecreto + 2;
-      console.log ( numeroSecreto );
-      alert("Te falta para llegar a tu numero secreto");
-      document.getElementById("intentos").value = contador;
-       }else if (numeroSecreto == numeroUsuario && contador == 0 && numeroSecreto == 100){
-        numeroSecreto = numeroSecreto -5;        
-          }else if (numeroSecreto == numeroUsuario && contador >= 1){
+      }else if (numeroSecreto == numeroUsuario && contador >= 1){
+        contador = contador + 1;
+        alert("¡¡¡Usted es un ganador!!! Y solo en " + contador + " intentos");
+        document.getElementById("intentos").value = contador;
+          }else if(numeroUsuario < numeroSecreto){
           contador = contador + 1;
-          alert("¡¡¡Usted es un ganador!!! Y solo en " + contador + " intentos");
+          alert("Te falta para llegar al numero secreto");
           document.getElementById("intentos").value = contador;
-            }else if(numeroUsuario < numeroSecreto){
+            }else if(numeroUsuario > numeroSecreto){
             contador = contador + 1;
-            alert("Te falta para llegar al numero secreto");
-            document.getElementById("intentos").value = contador;
-              }else if(numeroUsuario > numeroSecreto){
-              contador = contador + 1;
-              alert("Te pasaste de tu numero secreto");
-              document.getElementById("intentos").value = contador; 
-      }
+            alert("Te pasaste de tu numero secreto");
+            document.getElementById("intentos").value = contador;   
+        }
+        while(numeroSecreto == numeroUsuario && contador == 0){
+          numeroSecreto = Math.floor(Math.random() * 101);
+          console.log ( numeroSecreto );
+          alert("Te falta para llegar a tu numero");
+          contador = contador + 1;
+          document.getElementById("intentos").value = contador;
+        }
   } 
 
 
