3c2220b805f42c59ea24c0045794f6133ed909a4
agustinezequielgomez <agusgomez99@gmail.com>
2018-02-19 12:24:43 -0300

    Clase 4

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,11 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+while (numero > 9 || numero < 0){
 
+	numero = prompt("Ingrese un numero entre el 0 y el 10");
+
+}
+document.getElementById("Numero").value = numero;
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/recuperatorio/cinco.js
+++ 9-Parciales/recuperatorio/cinco.js
@@ -1,4 +1,20 @@
 function Mostrar()
 {
+    var mes;
+    mes = prompt("Ingresa un mes");
+    switch (mes){
 
+        case "Enero":
+        case "enero":
+        alert("Comienza el año");
+        break;
+
+        case "Diciembre":
+        case "diciembre":
+        alert("Se vienen las fiestas");
+        break;
+        
+default:
+alert("No es Enero ni Diciembre");
+    }
 }
